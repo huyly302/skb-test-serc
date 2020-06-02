4e15c7e40c5d9d49e57c7adc5d88dcc5c0c159b1
huy.ly <huy.ly@persol.co.jp>
2020-06-02 11:44:02 +0700

    add file with JP text for testing

---
+++ Serc001.java
@@ -0,0 +1,14 @@
+
+import java.io.Serializable;
+
+/**
+ *
+ * [クラス名]
+ *  信金フレームワーク専用定数クラス
+
+ */
+public class Global implements Serializable {
+   
+   // 改行コード
+   public static String RETURN_KEY_RN = "";
+}
